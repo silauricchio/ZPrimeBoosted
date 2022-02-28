1f78f9344acd77cf5536884beec69120fc62bae9
Silvia Auricchio <sauricch@lxplus717.cern.ch>
2022-02-28 18:32:11 +0100

    fixed upper limits

--- codes/StatisticalTests.py
+++ codes/StatisticalTests.py
@@ -1,11 +1,28 @@
 import ROOT
 import sys
 
+run_Asimov = True
+run_Toys = False
 
 poiValueForBackground = 0
 
-#Zprime_masses = ["400", "500", "750", "1000", "1250", "1500", "1750", "2000", "2250", "2500", "2750", "3000"]
-Zprime_masses = ["500", "750", "1000", "1250", "1500", "1750", "2000", "2250", "2500", "2750", "3000"]
+Zprime_masses = ["400", "500", "750", "1000", "1250", "1500", "1750", "2000", "2250", "2500", "2750", "3000"]
+#Zprime_masses = ["3000"]
+
+
+AsymLimits = {}
+ExpAsymLimits = {}
+ExpAsymLimits_m1s = {}
+ExpAsymLimits_p1s = {}
+ExpAsymLimits_m2s = {}
+ExpAsymLimits_p2s = {}
+
+ToysLimits = {}
+ExpToysLimits = {}
+ExpToysLimits_m1s = {}
+ExpToysLimits_p1s = {}
+ExpToysLimits_m2s = {}
+ExpToysLimits_p2s = {}
 
 
 for mass in Zprime_masses:
@@ -28,121 +45,170 @@ for mass in Zprime_masses:
         exit
 
     POI = mc.GetParametersOfInterest().first()
+    NP = mc.GetNuisanceParameters().first()
 
+    
     # ---------------------------------------------------
     # ----- Configure a ProfileLikelihoodCalculator -----
     # ---------------------------------------------------
-
-    plc = ROOT.RooStats.ProfileLikelihoodCalculator(data, model, ROOT.RooArgSet(POI)) #this class uses the Wilk's theorem and calculate p value with asymptotic formulae
-
-    '''
-    plc.SetConfidenceLevel(0.95)
-    interval = plc.GetInterval()
-    mu_best = interval.GetBestFitParameters().first()
-
-    lowerLimit = interval.LowerLimit(POI)
-    upperLimit = interval.UpperLimit(POI)
-    print("MU = " + str(mu_best))
     
 
-    #Make a plot
-    c = ROOT.TCanvas()
-    plot = ROOT.RooStats.LikelihoodIntervalPlot(interval)
-    plot.SetLineColor(ROOT.kWhite)
-    #plot.SetNPoints(50)  #do not use too many points, it could become very slow for some models
-    plot.Draw()
-    #plot.Draw("tf1") #use option TF1 if too slow (plot.Draw("tf1"))
-    c.SaveAs("../run/PLRScan_mu_Zp" + mass + ".pdf")
-    '''
-
-    # Get the significance using the GetHypoTest function:
-    # create a copy of the POI parameters to set the values to zero
-
-    nullparams = mc.GetParametersOfInterest().Clone()
-    nullparams.first().setVal(0)
-    plc.SetNullParameters(nullparams)
-
-    del nullparams
+    sbModel = mc.Clone()                                                                                                                                                                                  
+    sbModel.SetName('sbmodel')   
+    poi = sbModel.GetParametersOfInterest().first()                                                                                                                                                        
+    poi.setVal(1.)                                                                                                                                                                                         
+    sbModel.SetSnapshot(ROOT.RooArgSet(poi))                                                                                                                                                               
     
-    #Return the hypothesis test result obtained from the likelihood ratio of the maximum likelihood value with the null parameters fixed to their values, with respect keeping all parameters floating
-    testresult = plc.GetHypoTest()
-    significance = testresult.Significance()    
-    p_value = testresult.NullPValue() 
+    # Clone S+B model, set POI to zero and set as B-Only model                                                                                                                                             
+    bModel = sbModel.Clone()                                                                                                                                                                               
+    bModel.SetName('bmodel')                                                                                                                                                                               
+    poi_null = poi.Clone()                                                                                                                                                                                 
+    poi_null.setVal(0.)               
+    bModel.SetSnapshot(ROOT.RooArgSet(poi_null))         
     
 
-    print("Observed significance with asym formulae")
-    print("For Z'" + mass + " p value --> " + str(p_value) + " and Z --> " + str(significance))
+    if run_Asimov:
 
-    del plc
+        print('')
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('ASYMPTOTIC CALCULATION')
+        print('')
+        
+        AsymCalc = ROOT.RooStats.AsymptoticCalculator(data, bModel, sbModel, False)  
+        AsymCalc.SetOneSided(True)
+        
+        #Return the hypothesis test result obtained from the likelihood ratio of the maximum likelihood value with the null parameters fixed to their values, with respect keeping all parameters floating
+        testresult = AsymCalc.GetHypoTest()
+        significance = testresult.Significance()    
+        p_value = testresult.NullPValue() 
+        
+        print("Observed significance with asym formulae")
+        print("For Z'" + mass + " p value --> " + str(p_value) + " and Z --> " + str(significance))
+        
+        
+        fit_params = AsymCalc.GetBestFitParams() 
+        print(fit_params)
+        mu_best = AsymCalc.GetMuHat() 
+        
+        
+        # HypoTestInverter #per l'estrazione dei limiti                                                                                                                                                    
 
+        calc = ROOT.RooStats.HypoTestInverter(AsymCalc)                                                                                                                                                    
+        calc.SetConfidenceLevel(0.95)                                                                                                                                                                      
+        calc.UseCLs(True)                                                                                                                                                                                  
+        calc.SetVerbose(False)                                                                                                                                                                             
+        calc.SetAutoScan()                                                                                                                                                                                 
+        hypotestresult = calc.GetInterval()   
+        
+        ObsUpperLimit = hypotestresult.UpperLimit()
+        ExpUpperLimit = hypotestresult.GetExpectedUpperLimit(0)
+        ExpUpperLimit_m1s = hypotestresult.GetExpectedUpperLimit(-1)
+        ExpUpperLimit_p1s = hypotestresult.GetExpectedUpperLimit(1)
+        ExpUpperLimit_m2s = hypotestresult.GetExpectedUpperLimit(-2)
+        ExpUpperLimit_p2s = hypotestresult.GetExpectedUpperLimit(2)
+        
+        
+        print(mass)
+        print("MU up obs = " + str(ObsUpperLimit))
+        print("MU up exp = " + str(ExpUpperLimit))
+        print('') 
+        
+        AsymLimits['mass'] = ObsUpperLimit
+        ExpAsymLimits['mass'] = ExpUpperLimit
+        ExpAsymLimits_m1s['mass'] = ExpUpperLimit_m1s
+        ExpAsymLimits_p1s['mass'] = ExpUpperLimit_p1s
+        ExpAsymLimits_m2s['mass'] = ExpUpperLimit_m2s
+        ExpAsymLimits_p2s['mass'] = ExpUpperLimit_p2s
+        
+        del AsymCalc
+    
     
     # ---------------------------------------
     # ----- repeat the test with Toys MC ----
     # ---------------------------------------
-
-    NTOYS = 5000
-
-    # Get RooStats ModelConfig from workspace and save it as Signal+Background model
-    sbModel = mc.Clone()
-    poi = sbModel.GetParametersOfInterest().first()
-    poi.setVal(1.)
-    sbModel.SetSnapshot(ROOT.RooArgSet(poi))
     
-    # Clone S+B model, set POI to zero and set as B-Only model
-    bModel = sbModel.Clone()
-    bModel.SetName('bmodel')
-    poi_null = poi.Clone()
-    poi_null.setVal(0.)
-    bModel.SetSnapshot(ROOT.RooArgSet(poi_null))
+    if run_Toys:
+        print('')
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('TOYS CALCULATION')
+        print('')
+        
+        
+        NTOYS = 10000
+        
+        hc = ROOT.RooStats.FrequentistCalculator(data, bModel, sbModel) 
+        hc.SetToys(int(NTOYS), int(NTOYS))
+        hc.StoreFitInfo(True)
+        hc.UseSameAltToys()
+        
+        # Test statistics: profile likelihood
+        profll = ROOT.RooStats.ProfileLikelihoodTestStat(bModel.GetPdf())
+        profll.EnableDetailedOutput()
+        profll.SetLOffset(True)
+        profll.SetMinimizer('Minuit2')
+        profll.SetOneSided(True)
+        profll.SetPrintLevel(0)
+        profll.SetStrategy(2)
+        profll.SetAlwaysReuseNLL(True)
+        profll.SetReuseNLL(True)
+        
+        toymcs = hc.GetTestStatSampler()
+        toymcs.SetTestStatistic(profll)
+        toymcs_testresult = hc.GetHypoTest()
+        toymcs_significance = toymcs_testresult.Significance()
+        toymcs_p_value = toymcs_testresult.NullPValue()
+        
+        
+        print("Observed significance with Toys MC")
+        print("For Z'" + mass + " p value --> " + str(toymcs_p_value) + " and Z --> " + str(toymcs_significance))
+        exit()
+        
+        # HypoTestInverter #per l'estrazione dei limiti
+        toymc_calc = ROOT.RooStats.HypoTestInverter(hc)
+        toymc_calc.SetConfidenceLevel(0.95)
+        toymc_calc.UseCLs(True)
+        toymc_calc.SetVerbose(False)
+        toymc_calc.SetAutoScan()
+        toymc_hypotestresult = toymc_calc.GetInterval()
+        
+        toymc_ObsUpperLimit = toymc_hypotestresult.UpperLimit()
+        toymc_ExpUpperLimit = toymc_hypotestresult.GetExpectedUpperLimit(0)
+        toymc_ExpUpperLimit_m1s = toymc_hypotestresult.GetExpectedUpperLimit(-1)
+        toymc_ExpUpperLimit_p1s = toymc_hypotestresult.GetExpectedUpperLimit(1)
+        toymc_ExpUpperLimit_m2s = toymc_hypotestresult.GetExpectedUpperLimit(-2)
+        toymc_ExpUpperLimit_p2s = toymc_hypotestresult.GetExpectedUpperLimit(2)
+        
+        print(mass)
+        print("MU up obs = " + str(toymc_ObsUpperLimit))
+        print("MU up exp = " + str(toymc_ExpUpperLimit))
+        print('')
+        
+        ToysLimits['mass'] = toymc_ObsUpperLimit
+        ExpToysLimits['mass'] = toymc_ExpUpperLimit
+        ExpToysLimits_m1s['mass'] = toymc_ExpUpperLimit_m1s
+        ExpToysLimits_p1s['mass'] = toymc_ExpUpperLimit_p1s
+        ExpToysLimits_m2s['mass'] = toymc_ExpUpperLimit_m2s
+        ExpToysLimits_p2s['mass'] = toymc_ExpUpperLimit_p2s
+        
+        del hc
     
-    hc = ROOT.RooStats.FrequentistCalculator(data, bModel, sbModel)
-    hc.SetToys(int(NTOYS), int(NTOYS/2.))
-    hc.StoreFitInfo(True)
-    hc.UseSameAltToys()
-
-    # Test statistics: profile likelihood
-    profll = ROOT.RooStats.ProfileLikelihoodTestStat(bModel.GetPdf())
-    profll.EnableDetailedOutput()
-    profll.SetLOffset(True)
-    profll.SetMinimizer('Minuit2')
-    profll.SetOneSided(True)
-    profll.SetPrintLevel(0)
-    profll.SetStrategy(2)
-    profll.SetAlwaysReuseNLL(True)
-    profll.SetReuseNLL(True)
-    
-    toymcs = hc.GetTestStatSampler()
-    toymcs.SetTestStatistic(profll)
-    toymcs_testresult = hc.GetHypoTest()
-    toymcs_significance = toymcs_testresult.Significance()
-    toymcs_p_value = toymcs_testresult.NullPValue()
 
 
-    print("Observed significance with Toys MC")
-    print("For Z'" + mass + " p value --> " + str(toymcs_p_value) + " and Z --> " + str(toymcs_significance))
+#p_value_graph_asym = ROOT.TGraph("p value", "p value", masses, p_values)
+#significance_graph_asym = 
+#p_value_graph_toys =
+#significance_graph_toys =
 
+#upper_limits_asym= brazilian plot
+#upper_limits_toys=
 
-    # HypoTestInverter #per l'estrazione dei limiti
-    calc = ROOT.RooStats.HypoTestInverter(hc)
-    calc.SetConfidenceLevel(0.95)
-    calc.UseCLs(True)
-    calc.SetVerbose(False)
-    calc.SetAutoScan()
-    #calc.SetFixedScan(6, 0., 10., False)
-    hypotestresult = calc.GetInterval()
-    
-    # Plot HypoTestInverter result
-    c1 = ROOT.TCanvas()
-    plot = ROOT.RooStats.HypoTestInverterPlot('hypotest_inverter', 'hypotest inverter', hypotestresult)
-    plot.MakePlot()
-    plot.MakeExpectedPlot()
-    plot.Draw('CLB2CL')
-    c1.SaveAs("../run/boh_" + mass + ".pdf")
+'''
+# Plot HypoTestInverter result
+c1 = ROOT.TCanvas()
+plot = ROOT.RooStats.HypoTestInverterPlot('hypotest_inverter', 'hypotest inverter', hypotestresult)
+plot.MakePlot()
+plot.MakeExpectedPlot()
+plot.Draw('CLB2CL')
+c1.SaveAs("../run/boh_" + mass + ".pdf")
+'''
 
-    # Print information
-    hc.GetFitInfo().Print('v')
-    
-
-    del hc
-
-    
